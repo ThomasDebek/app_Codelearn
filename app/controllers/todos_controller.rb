773dc8e18b09f2d18c5e24f02704cf2345362d58
Tomasz Dębek <tdebek@Mac-mini-Tomasz.local>
2014-09-29 13:13:39 +0200

    Przykladowe api - wysylanie formularza

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,16 @@
+class TodosController < ApplicationController
+  def index
+    @todo_items = Todo.all
+    render :index
+  end
+
+  def delete
+  end
+
+  def add
+    Todo.create(:todo_item => params[:todo_text])
+    redirect_to :action => 'index'
+  end
+
+
+end
